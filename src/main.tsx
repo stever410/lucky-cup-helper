54b9fc985f6dab33dc15a3620c5289360ac53c30
stever410 <ngotanduc410@gmail.com>
2023-06-13 11:42:29 +0700

    Add PWA functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { registerSW } from "virtual:pwa-register";
+
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["vite.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#0d6efd",
+        background_color: "#fff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "Lucky Cup Helper",
+        short_name: "LCH",
+        description: "A lucky cup game helper website",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
