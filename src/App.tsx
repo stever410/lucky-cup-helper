90cc48bbf89c9a4e8b78b551ebac17bf22bc29a4
stever410 <ngotanduc410@gmail.com>
2023-06-13 12:29:59 +0700

    Update typo and layout

--- src/App.tsx
+++ src/App.tsx
@@ -29,36 +29,43 @@ const App = () => {
     <Container fluid>
       <Stack gap={3} className="d-flex justify-content-center">
         <h1 className="text-center">Lucky Cup Helper</h1>
-        <ButtonGroup>
-          <Button variant="outline-primary" onClick={addRow}>
-            Thêm dòng
-          </Button>
-          <Button variant="outline-primary" onClick={addColumn}>
-            Thêm cột
-          </Button>
-          <Button variant="outline-danger" onClick={removeLastRow}>
-            Xóa dòng cuối
-          </Button>
-          <Button variant="outline-danger" onClick={removeLastColumn}>
-            Xóa cột cuối
-          </Button>
-        </ButtonGroup>
+        <div className="d-flex justify-content-center">
+          <ButtonGroup className="w-75">
+            <Button variant="outline-primary" onClick={addRow}>
+              Thêm dòng
+            </Button>
+            <Button variant="outline-primary" onClick={addColumn}>
+              Thêm cột
+            </Button>
+            <Button variant="outline-danger" onClick={removeLastRow}>
+              Xóa dòng cuối
+            </Button>
+            <Button variant="outline-danger" onClick={removeLastColumn}>
+              Xóa cột cuối
+            </Button>
+          </ButtonGroup>
+        </div>
         <Spreadsheet
           className="spreadsheet-container"
           data={matrix}
           onChange={handleChange}
         />
-        <ButtonGroup className="">
-          <Button variant="outline-primary" onClick={saveMatrixToLocalStorage}>
-            Lưu
-          </Button>
-          <Button variant="outline-danger" onClick={clearMatrix}>
-            Xóa toàn bộ giá trị
-          </Button>
-        </ButtonGroup>
+        <div className="d-flex justify-content-center">
+          <ButtonGroup className="w-25">
+            <Button
+              variant="outline-primary"
+              onClick={saveMatrixToLocalStorage}
+            >
+              Lưu
+            </Button>
+            <Button variant="outline-danger" onClick={clearMatrix}>
+              Xóa
+            </Button>
+          </ButtonGroup>
+        </div>
         <Row className="gx-4 gy-4 mx-0">
           {statistic.map(({ title, value }) => (
-            <Col key={title} xs={12} sm={6} lg={4}>
+            <Col key={title} xs={12} sm={6} lg={4} xl={3}>
               <CustomCard title={title} body={value.toString()} />
             </Col>
           ))}
